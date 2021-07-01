4df3efb566b1dc37ac7518c7b6dfea6ec7494a9c
nmusaelian-rally <nik.musaelian@broadcom.com>
2021-07-01 16:46:12 -0600

    second commit

--- src/components/Simple.js
+++ src/components/Simple.js
@@ -5,13 +5,14 @@ import useKeyboardShortcut from '../utils/use-hotkey';
 
 const Simple = () => {
     const [foo, setFoo] = useState('foo');
-    const keys = ['control','shift','f']; 
+    //const keys = ['control','shift','f']; 
+    const keys = ['Meta','f']; 
     //'Control', 'CONTROL', 'control' work; 'ctrl', 'CTRL' does not 
     const handleKeyboardShortcut = useCallback(() => {
         console.log('detected keyboard shortcut')
         setFoo(current => current+'o')
       }, [setFoo])
-    useKeyboardShortcut(keys, handleKeyboardShortcut)
+    useKeyboardShortcut(keys, handleKeyboardShortcut, { overrideSystem: true })
     return (
         <div>{foo}</div>
     )
