a9a1d96604a5ccdc8b69e9c5fb6208819de8b23f
nmusaelian-rally <nik.musaelian@broadcom.com>
2021-07-06 18:40:23 -0600

    added input box

--- src/components/Simple.js
+++ src/components/Simple.js
@@ -1,20 +1,27 @@
 
-import React, { useState, useCallback } from 'react';
+import React, { Fragment, useState, useCallback } from 'react';
 
 import useKeyboardShortcut from '../utils/use-hotkey';
 
 const Simple = () => {
+    console.log('loaded Simple component')
     const [foo, setFoo] = useState('foo');
-    //const keys = ['control','shift','f']; 
-    const keys = ['Meta','f']; 
-    //'Control', 'CONTROL', 'control' work; 'ctrl', 'CTRL' does not 
+    const handleInput = e => {
+        setFoo(e.target.value)
+    }
+    const keys = ['Control', 'Shift','F']; 
     const handleKeyboardShortcut = useCallback(() => {
         console.log('detected keyboard shortcut')
         setFoo(current => current+'o')
       }, [setFoo])
-    useKeyboardShortcut(keys, handleKeyboardShortcut, { overrideSystem: true })
+    useKeyboardShortcut(keys, handleKeyboardShortcut, { overrideSystem: true }) 
     return (
-        <div>{foo}</div>
+        <Fragment>
+          <div>{foo}</div>
+          <label htmlFor="box">type here:</label>
+          <input type="input" onChange={handleInput}/>
+        </Fragment>
+
     )
 }
 export default Simple;
